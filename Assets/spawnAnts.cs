35013418e1d1c4671ee80adae3ff122fc19b727b
Pizmovc <pizmovc@gmail.com>
2015-03-14 20:00:42 +0100

    Add some randomnes factor to spawning delay

--- Assets/spawnAnts.cs
+++ Assets/spawnAnts.cs
@@ -14,15 +14,16 @@ public class spawnAnts : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 
 	IEnumerator waitAndSpawn(float delayTime){
+		Debug.Log ("Delay in seconds before spawning: "+ delayTime);
+		yield return new WaitForSeconds(delayTime);
 		if (foodAmount > 0) {
 			foodAmount--;
 			Instantiate (antPrefab, new Vector2 (Random.Range (-5.0f, 5.0f), Random.Range (-5.0f, 5.0f)), Quaternion.identity);
 		}
-		yield return new WaitForSeconds(delayTime);
-		StartCoroutine(waitAndSpawn (delayTime));
+		StartCoroutine(waitAndSpawn (spawnDelayTime + Random.Range(-1.0f,1.0f)));
 	}
 }
